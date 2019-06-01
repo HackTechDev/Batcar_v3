905f964400ae37091fab9e0d802249b980c1de1a
Nekrofage <lesanglierdesardennes@gmail.com>
2019-06-01 10:29:18 +0200

    Formattating

--- recv/recv.ino
+++ recv/recv.ino
@@ -24,26 +24,27 @@ const int pinRecv = 2;              // ir receiver connect to D2
 void setup() {
     Serial.begin(115200);
 
- Serial.println("Init Servo"); 
+    // Servo
+    Serial.println("Init Servo"); 
 
-  servoClaw.attach(7);
-  servoTail1.attach(8);
-  servoTail2.attach(9);
-  servoTail3.attach(10);
+    servoClaw.attach(7);
+    servoTail1.attach(8);
+    servoTail2.attach(9);
+    servoTail3.attach(10);
 
 
-  servoClaw.write(40);
-  delay(1000);
-  servoClaw.write(140);
-  delay(1000);
-  servoClaw.write(40);
+    servoClaw.write(40);
+    delay(1000);
+    servoClaw.write(140);
+    delay(1000);
+    servoClaw.write(40);
 
-    
-  turnServo(servoTail1, 90);
-  turnServo(servoTail2, 90);
-  turnServo(servoTail3, 90);
 
-    
+    turnServo(servoTail1, 90);
+    turnServo(servoTail2, 90);
+    turnServo(servoTail3, 90);
+
+    // Infrared
     Serial.println("Init Infrared");
     IR.Init(pinRecv);
 
@@ -58,90 +59,89 @@ void loop() {
         // receive data to dta
         IR.Recv(dta);               
         irdata = dta[BIT_DATA];
-      
-         if(irdata == 1) {
-            Serial.println("1: Claw close");
-             servoClaw.write(90);
-         }
 
-         if(irdata == 2) {
+        if(irdata == 1) {
+            Serial.println("1: Claw close");
+            servoClaw.write(90);
+        }
+
+        if(irdata == 2) {
             Serial.println("2: Claw open");
             servoClaw.write(150);
-         }    
+        }    
 
 
-         if(irdata == 3) {
+        if(irdata == 3) {
             Serial.println("3: Tail 1 +");
-           int angle = servoTail1.read();
-           if ( angle == 10) {
-             turnServo(servoTail1, 90);
-           }
-           if ( angle == 90) {
-             turnServo(servoTail1, 170);
-           }            
-         }
+            int angle = servoTail1.read();
+            if ( angle == 10) {
+                turnServo(servoTail1, 90);
+            }
+            if ( angle == 90) {
+                turnServo(servoTail1, 170);
+            }            
+        }
 
-         if(irdata == 4) {
+        if(irdata == 4) {
             Serial.println("4: Tail 1 -");
-               int angle = servoTail1.read();
-               if ( angle == 170) {
-                 turnServo(servoTail1, 90);
-               }
-               if ( angle == 90) {
-                 turnServo(servoTail1, 10);
-               }
-         }   
+            int angle = servoTail1.read();
+            if ( angle == 170) {
+                turnServo(servoTail1, 90);
+            }
+            if ( angle == 90) {
+                turnServo(servoTail1, 10);
+            }
+        }   
 
-         
-         if(irdata == 5) {
+        if(irdata == 5) {
             Serial.println("5: Tail 2 +");
-               int angle = servoTail2.read();
-               if ( angle == 10) {
-                 turnServo(servoTail2, 90);
-               }
-               if ( angle == 90) {
-                 turnServo(servoTail2, 120);
-               }            
-         }
+            int angle = servoTail2.read();
+            if ( angle == 10) {
+                turnServo(servoTail2, 90);
+            }
+            if ( angle == 90) {
+                turnServo(servoTail2, 120);
+            }            
+        }
 
-         if(irdata == 6) {
+        if(irdata == 6) {
             Serial.println("6: Tail 2 -");
-                int angle = servoTail2.read();
-               if ( angle == 120) {
-                 turnServo(servoTail2, 90);
-               }
-               if ( angle == 90) {
-                 turnServo(servoTail2, 10);
-               }           
-         }   
+            int angle = servoTail2.read();
+            if ( angle == 120) {
+                turnServo(servoTail2, 90);
+            }
+            if ( angle == 90) {
+                turnServo(servoTail2, 10);
+            }           
+        }   
 
 
-         if(irdata == 7) {
+        if(irdata == 7) {
             Serial.println("7: Tail 3 +");
-                 int angle = servoTail3.read();
-               if ( angle == 20) {
-                 turnServo(servoTail3, 90);
-               }
-               if ( angle == 90) {
-                 turnServo(servoTail3, 160);
-               }          
-         }
+            int angle = servoTail3.read();
+            if ( angle == 20) {
+                turnServo(servoTail3, 90);
+            }
+            if ( angle == 90) {
+                turnServo(servoTail3, 160);
+            }          
+        }
 
-         if(irdata == 8) {
+        if(irdata == 8) {
             Serial.println("8: Tail 3 -");
-                int angle = servoTail3.read();
-               if ( angle == 160) {
-                 turnServo(servoTail3, 90);
-               }
-               if ( angle == 90) {
-                 turnServo(servoTail3, 20);
-               }           
-         }   
-         
-             
+            int angle = servoTail3.read();
+            if ( angle == 160) {
+                turnServo(servoTail3, 90);
+            }
+            if ( angle == 90) {
+                turnServo(servoTail3, 20);
+            }           
+        }   
+
+
     }
 }
 
 void turnServo(Servo servoMotor,int value){
-  servoMotor.write(value);
+    servoMotor.write(value);
 }
--- send/send.ino
+++ send/send.ino
@@ -41,9 +41,9 @@ void dtaInit() {
     dtaSend[BIT_START_L]    = 90;     // the logic low duration of "Start"
     dtaSend[BIT_DATA_H]     = 11;     // the logic "long" duration in the communication
     dtaSend[BIT_DATA_L]     = 33;     // the logic "short" duration in the communication
-    
+
     dtaSend[BIT_DATA_LEN]   = 1;      // Number of data which will sent. If the number is other, you should increase or reduce dtaSend[BIT_DATA+x].
-    
+
     dtaSend[BIT_DATA+0]     = 128;      // data that will sent
 }
 
@@ -51,221 +51,162 @@ void dtaInit() {
 
 
 void setup() {
-  // Infrared
-  Serial.begin(38400);
-  dtaInit();
+    // Infrared
+    Serial.begin(38400);
+    dtaInit();
 
-  // Init Servo
+    // Init Servo
 
-  motors.setLeftSpeed(0);
-  motors.setRightSpeed(0);
-  
-  // Bluetooth
-  pinMode(RxD, INPUT);
-  pinMode(TxD, OUTPUT);  
-  setupBlueToothConnection(); 
+    motors.setLeftSpeed(0);
+    motors.setRightSpeed(0);
+
+    // Bluetooth
+    pinMode(RxD, INPUT);
+    pinMode(TxD, OUTPUT);  
+    setupBlueToothConnection(); 
 }
 
 void loop() {
 
-  char recvChar;
+    char recvChar;
 
-  while(1) {
+    while(1) {
 
-    // Bluetooth
-    if (blueToothSerial.available()) {
-      recvChar = blueToothSerial.read();
-      Serial.print(recvChar);
+        // Bluetooth
+        if (blueToothSerial.available()) {
+            recvChar = blueToothSerial.read();
+            Serial.print(recvChar);
 
-        if(recvChar == '8') {
-           Serial.println("Move forward");
-           for (int speed = 0; speed <= 100; speed++){
-             motors.setLeftSpeed(speed);
-             motors.setRightSpeed(speed);
-             delay(2);
-           } 
+            if(recvChar == '8') {
+                Serial.println("Move forward");
+                for (int speed = 0; speed <= 100; speed++){
+                    motors.setLeftSpeed(speed);
+                    motors.setRightSpeed(speed);
+                    delay(2);
+                } 
+            }
+
+            if(recvChar == '2') {
+                Serial.println("Move backward");
+                for (int speed = 0; speed >= -100; speed--) {
+                    motors.setLeftSpeed(speed);
+                    motors.setRightSpeed(speed);
+                    delay(2);
+                }
+            }
+
+            if(recvChar == '7') {
+                Serial.println("Move backward right");
+                for (int speed = 0; speed <= 100; speed++) {
+                    motors.setLeftSpeed(speed);
+
+                    delay(2);
+                }
+            }
+
+            if(recvChar == '9') {
+                Serial.println("Move backward left");
+                for (int speed = 0; speed <= 100; speed++) {
+                    motors.setRightSpeed(speed);
+                    delay(2);
+                }
+            }
+
+            if(recvChar == '1') {
+                Serial.println("Move forward left");
+                for (int speed = 0; speed >= -100; speed--) {
+                    motors.setLeftSpeed(speed);
+
+                    delay(2);
+                }  
+            }
+
+            if(recvChar == '3') {
+                Serial.println("Move forward right");
+                for (int speed = 0; speed >= -100; speed--) {
+
+                    motors.setRightSpeed(speed);
+                    delay(2);
+                }
+            }
+
+            if(recvChar == '5') {
+                Serial.println("Move Stop");
+
+                motors.setLeftSpeed(0);
+                motors.setRightSpeed(0);
+                delay(2);
+            }
+
+            // Claw
+            if(recvChar == 'z') {
+                dtaSend[BIT_DATA] = 1;
+                IR.Send(dtaSend, 38);          
+                Serial.println("Claw open");
+            }
+
+            if(recvChar == 's') {
+                dtaSend[BIT_DATA] = 2;
+                IR.Send(dtaSend, 38);
+                Serial.println("Claw open");
+            }
+
+            // Tail 1
+            if(recvChar == 'e') {
+                dtaSend[BIT_DATA] = 3;
+                IR.Send(dtaSend, 38);          
+                Serial.println("3: Tail 1 +");
+            }
+
+            if(recvChar == 'd') {
+                dtaSend[BIT_DATA] = 4;
+                IR.Send(dtaSend, 38);
+                Serial.println("4: Tail 1 -");
+            }
+
+            // Tail 2
+            if(recvChar == 'r') {
+                dtaSend[BIT_DATA] = 5;
+                IR.Send(dtaSend, 38);          
+                Serial.println("5: Tail 2 +");
+            }
+
+            if(recvChar == 'f') {
+                dtaSend[BIT_DATA] = 6;
+                IR.Send(dtaSend, 38);
+                Serial.println("6: Tail 2 -");
+            }
+
+            // Tail 3
+            if(recvChar == 't') {
+                dtaSend[BIT_DATA] = 7;
+                IR.Send(dtaSend, 38);          
+                Serial.println("7: Tail 3 +");
+            }
+
+            if(recvChar == 'g') {
+                dtaSend[BIT_DATA] = 8;
+                IR.Send(dtaSend, 38);
+                Serial.println("8: Tail 3 -");
+            }
+
         }
-
-
-        if(recvChar == '2') {
-           Serial.println("Move backward");
-           for (int speed = 0; speed >= -100; speed--)
-           {
-             motors.setLeftSpeed(speed);
-             motors.setRightSpeed(speed);
-             delay(2);
-           }
-     
-        }
-
-
-        if(recvChar == '7') {
-           Serial.println("Move backward right");
-                         for (int speed = 0; speed <= 100; speed++){
-                 motors.setLeftSpeed(speed);
-
-                 delay(2);
-               }
-          
-        }
-
-
-        if(recvChar == '9') {
-           Serial.println("Move backward left");
-                         for (int speed = 0; speed <= 100; speed++){
-                 motors.setRightSpeed(speed);
-                 delay(2);
-               }
-          
-        }
-
-
-        if(recvChar == '1') {
-           Serial.println("Move forward left");
-                       for (int speed = 0; speed >= -100; speed--)
-               {
-                 motors.setLeftSpeed(speed);
-
-                 delay(2);
-               }  
-        }
-
-
-        if(recvChar == '3') {
-           Serial.println("Move forward right");
-                         for (int speed = 0; speed >= -100; speed--)
-               {
-
-                 motors.setRightSpeed(speed);
-                 delay(2);
-               }
-        }
-
-
-        if(recvChar == '5') {
-           Serial.println("Move Stop");
-                        
-                 motors.setLeftSpeed(0);
-                 motors.setRightSpeed(0);
-                 delay(2);
-               
-        }
-
-
-
-      
-        // Claw
-        if(recvChar == 'z') {
-          dtaSend[BIT_DATA] = 1;
-          IR.Send(dtaSend, 38);          
-          Serial.println("Claw open");
-        }
-
-        if(recvChar == 's') {
-          dtaSend[BIT_DATA] = 2;
-          IR.Send(dtaSend, 38);
-          Serial.println("Claw open");
-        }
-
-        // Tail 1
-        if(recvChar == 'e') {
-          dtaSend[BIT_DATA] = 3;
-          IR.Send(dtaSend, 38);          
-          Serial.println("3: Tail 1 +");
-        }
-
-        if(recvChar == 'd') {
-          dtaSend[BIT_DATA] = 4;
-          IR.Send(dtaSend, 38);
-          Serial.println("4: Tail 1 -");
-        }
-
-        // Tail 2
-        if(recvChar == 'r') {
-          dtaSend[BIT_DATA] = 5;
-          IR.Send(dtaSend, 38);          
-          Serial.println("5: Tail 2 +");
-        }
-
-        if(recvChar == 'f') {
-          dtaSend[BIT_DATA] = 6;
-          IR.Send(dtaSend, 38);
-          Serial.println("6: Tail 2 -");
-        }
-
-        // Tail 3
-        if(recvChar == 't') {
-          dtaSend[BIT_DATA] = 7;
-          IR.Send(dtaSend, 38);          
-          Serial.println("7: Tail 3 +");
-        }
-
-        if(recvChar == 'g') {
-          dtaSend[BIT_DATA] = 8;
-          IR.Send(dtaSend, 38);
-          Serial.println("8: Tail 3 -");
-        }
-
-        
-      }
-    
-  }
-  delay(500);
-  /*
-      dtaSend[BIT_DATA] = 1;
-      IR.Send(dtaSend, 38);
-      
-      delay(2000);
-  
-      dtaSend[BIT_DATA] = 2;
-      IR.Send(dtaSend, 38);
-          
-      delay(2000);
-  
-      dtaSend[BIT_DATA] = 3;
-      IR.Send(dtaSend, 38);
-      
-      delay(2000);
-  
-      dtaSend[BIT_DATA] = 4;
-      IR.Send(dtaSend, 38);
-      
-      delay(2000);
-  
-      dtaSend[BIT_DATA] = 5;
-      IR.Send(dtaSend, 38);
-          
-      delay(2000);
-  
-      dtaSend[BIT_DATA] = 6;
-      IR.Send(dtaSend, 38);
-      
-      delay(2000);
-  
-      dtaSend[BIT_DATA] = 7;
-      IR.Send(dtaSend, 38);
-          
-      delay(2000);
-  
-      dtaSend[BIT_DATA] = 8;
-      IR.Send(dtaSend, 38);
-  
-     delay(2000);
-  */
+    }
+    delay(500);
 }
 
 
 void setupBlueToothConnection() {
-  Serial.println("setupBlueToothConnection");
-  blueToothSerial.begin(38400);
-  blueToothSerial.print("\r\n+STWMOD=0\r\n");
-  blueToothSerial.print("\r\n+STNA=Batcar\r\n");
-  blueToothSerial.print("\r\n+STPIN=6666\r\n");
-  blueToothSerial.print("\r\n+STOAUT=1\r\n");
-  blueToothSerial.print("\r\n+STAUTO=0\r\n");
-  delay(2000);
-  blueToothSerial.print("\r\n+INQ=1\r\n");
-  Serial.println("The slave bluetooth is inquirable!");
-  delay(2000);
-  blueToothSerial.flush();
+    Serial.println("setupBlueToothConnection");
+    blueToothSerial.begin(38400);
+    blueToothSerial.print("\r\n+STWMOD=0\r\n");
+    blueToothSerial.print("\r\n+STNA=Batcar\r\n");
+    blueToothSerial.print("\r\n+STPIN=6666\r\n");
+    blueToothSerial.print("\r\n+STOAUT=1\r\n");
+    blueToothSerial.print("\r\n+STAUTO=0\r\n");
+    delay(2000);
+    blueToothSerial.print("\r\n+INQ=1\r\n");
+    Serial.println("The slave bluetooth is inquirable!");
+    delay(2000);
+    blueToothSerial.flush();
 } 
